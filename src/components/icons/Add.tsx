21d4f12bb53e3f7ccbe419ac4d95da6bfccc4065
Jakub Wąsowski <jakub@nerdz.pl>
2023-05-11 20:21:47 +0200

    feat: create app header

---
+++ src/components/icons/Add.tsx
@@ -0,0 +1,18 @@
+export const Add = () => (
+  <svg
+    width="24"
+    height="25"
+    viewBox="0 0 24 25"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M17 12.5H7" stroke="white" strokeLinecap="round" />
+    <path d="M12 17.5V7.5" stroke="white" strokeLinecap="round" />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M12 22.5C17.5228 22.5 22 18.0228 22 12.5C22 6.97715 17.5228 2.5 12 2.5C6.47715 2.5 2 6.97715 2 12.5C2 18.0228 6.47715 22.5 12 22.5Z"
+      stroke="white"
+    />
+  </svg>
+);
