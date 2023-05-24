d2e517949c919b2540d220512a385dd0c961c3a0
Wiecek-K <krystiandwiecek@gmail.com>
2023-05-25 01:42:59 +0200

    clean css code

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -16,32 +16,23 @@ export const CardComponent = ({ front, back }: CardComponentProps) => {
     if (isFront) {
       if (refBack.current && refFront.current) {
         refBack.current.classList.add(css.fadeOut)
-        refBack.current.classList.remove(css.fadeIn)
       }
       setTimeout(() => {
         if (refBack.current && refFront.current) {
-          refBack.current.classList.add(css.backfaceHidden)
-          refBack.current.classList.remove(css.backfaceVisible)
-          refFront.current.classList.add(css.backfaceVisible)
-          refFront.current.classList.remove(css.backfaceHidden)
+          refBack.current.classList.add(css.hidden)
+          refFront.current.classList.remove(css.hiddenn)
           refFront.current.classList.remove(css.fadeOut)
-          refFront.current.classList.add(css.fadeIn)
         }
       }, 600) //~~half time of css.card transform transition
     } else {
       if (refFront.current) {
         refFront.current.classList.add(css.fadeOut)
-        refFront.current.classList.remove(css.fadeIn)
       }
       setTimeout(() => {
         if (refBack.current && refFront.current) {
-          refFront.current.classList.add(css.backfaceHidden)
-          refFront.current.classList.remove(css.backfaceVisible)
-          refBack.current.classList.remove(css.backfaceHidden)
-          refBack.current.classList.add(css.backfaceVisible)
+          refFront.current.classList.add(css.hidden)
+          refBack.current.classList.remove(css.hidden)
           refBack.current.classList.remove(css.fadeOut)
-          refBack.current.classList.add(css.fadeIn)
-          console.log('into abck')
         }
       }, 600)
     }
@@ -54,7 +45,7 @@ export const CardComponent = ({ front, back }: CardComponentProps) => {
       <div ref={refFront} className={css.front}>
         <CardOneSite cardText={front} />
       </div>
-      <div className={`${css.back} ${css.backfaceHidden}`} ref={refBack}>
+      <div className={`${css.back} ${css.hidden}`} ref={refBack}>
         <CardOneSite cardText={back} />
       </div>
     </div>
