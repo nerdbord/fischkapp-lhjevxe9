3af08bdf7414ad61b577f4260a1d340895705358
Wiecek-K <krystiandwiecek@gmail.com>
2023-06-02 04:09:29 +0200

    fixes

--- src/components/AppContainer.tsx
+++ src/components/AppContainer.tsx
@@ -2,6 +2,6 @@ import { PropsWithChildren } from 'react'
 
 import styles from './AppContainer.module.css'
 
-export const AppContainer = ({ children }): PropsWithChildren<object> => (
+export const AppContainer = ({ children }: PropsWithChildren<object>) => (
   <div className={styles.container}>{children}</div>
 )
