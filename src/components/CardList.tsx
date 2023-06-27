4a70c444845bf0a9708ec2d49f452617362e0bd5
Wiecek-K <krystiandwiecek@gmail.com>
2023-06-27 16:21:05 +0200

    finish task

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -4,16 +4,22 @@ import { FlashCardI } from '../types/types'
 
 interface Cards {
   cardsData: FlashCardI[]
+  handleDeleteBtn(cardIdToRemove: string): void
 }
 
-export function CardList({ cardsData }: Cards) {
+export function CardList({ cardsData, handleDeleteBtn }: Cards) {
   const clonedArray = [...cardsData]
 
   return (
     <ul className={styles['card-list']}>
       {clonedArray.reverse().map((card) => (
         <li key={card._id}>
-          <CardComponent front={card.front} back={card.back} id={card._id} />
+          <CardComponent
+            front={card.front}
+            back={card.back}
+            id={card._id}
+            handleDeleteBtn={handleDeleteBtn}
+          />
         </li>
       ))}
     </ul>
