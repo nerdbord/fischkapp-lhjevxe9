17fa44c1ae5f331555d4aff2553db524810a5f5e
Wiecek-K <krystiandwiecek@gmail.com>
2023-06-27 20:01:50 +0200

    fix delpoy

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/fischkapp-lhjevxe9/',
-  root: './',
-  build: {
-    outDir: 'dist',
-  },
 })
