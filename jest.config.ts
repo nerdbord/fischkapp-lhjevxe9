70850a9ba210f8e9cbb93061259c0ca2448680d3
Wiecek-K <krystiandwiecek@gmail.com>
2023-09-08 04:15:51 +0200

    checkpoint- tests works - fetch problem

--- jest.config.ts
+++ jest.config.ts
@@ -4,8 +4,10 @@ export default {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
     // process `*.tsx` files with `ts-jest`
+    '.+\\.(css|styl|less|sass|scss)$': 'jest-css-modules-transform',
   },
   moduleNameMapper: {
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
   },
+
 }
